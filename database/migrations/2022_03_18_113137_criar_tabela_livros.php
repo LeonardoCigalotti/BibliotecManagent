bec257a53a51073641eea4a59c36edd9ea0b55e7
Leonardo <daniel.brigida@tmax.com.br>
2022-03-18 11:39:10 -0300

    config modificado para sincronizar com banco de dados

---
+++ database/migrations/2022_03_18_113137_criar_tabela_livros.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CriarTabelaLivros extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('livros', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('title');
+            $table->string('descricao');
+            $table->string('autor');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('livros');
+    }
+}
