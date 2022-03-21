8bd40608eaaa7b0fe578b9d8c5aba6a1cc5be75d
Leonardo <daniel.brigida@tmax.com.br>
2022-03-21 10:51:39 -0300

    novo layout da tela login, novo css da tela login e exclusão do antigo css do login

--- resources/views/Usuarios/login.blade.php
+++ resources/views/Usuarios/login.blade.php
@@ -1,34 +1,20 @@
-@extends('navbar')
+@extends('layoutInicio')
 
 @section('css')
-<link rel="stylesheet" type="text/css" href="{{url('css/login.css')}}">
-@endsection
-
-@section('header')
-    Entrar/login
+<link rel="stylesheet" type="text/css" href="{{url('css/layoutInicio.css')}}">
 @endsection
 
 @section('body')
 
-    <form method="post">
-        @csrf
-        <div class="form-group">
-            <label for="email">E-mail: </label>
-            <input type="email" name="email" id="email" required>
-        </div>
-
-        <div class="form-group">
-            <label for="password">Senha: </label>
-            <input type="password" name="password" id="password" required>
-        </div>
-
-        <button type="submit" class="entrar">Entrar</button>
-    </form>
-    <a href="#">
-        <button class="registrar">
-            Registrar-se
-        </button>
-    </a>
-
+@section('header')
+Entrar/Login
+@endsection
+            <form>
+                <label for="email">E-mail:</label>
+                <input type="tex" id="email"><br>
+                <label for="password">Senha:</label>
+                <input type="password" id="password"><br><br>
+                <button type="submit">Entrar</button>
+            </form>
 @endsection
 
